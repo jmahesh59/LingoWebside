f257960beaf99baca2abed9df12c3c24aad77d01
mahesh <jadhao.mahesh1009@gmail.com>
2024-03-21 11:31:00 +0530

    add footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn , practice, and Master new languages with Lingo
         </h1>
-      </div>
-      <div className="">
+   
+      <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
         <ClerkLoading>
           <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
         </ClerkLoading>
@@ -56,6 +56,7 @@ export default function Home() {
           </SignedIn>
         </ClerkLoaded>
       </div>
+    </div>  
    </div>
   );
 }
