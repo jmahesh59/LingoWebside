d62a0aa9e1f64cb47b71d4ba17d0b95e0183c154
mahesh <jadhao.mahesh1009@gmail.com>
2024-03-25 11:05:05 +0530

    card page

---
+++ db/queries.ts
@@ -0,0 +1,26 @@
+import { cache } from "react";
+import db from "./drizzle";
+import { auth } from "@clerk/nextjs";
+import { userProgress } from "./schema";
+import { eq } from "drizzle-orm";
+
+export const getCourses = cache(async ()=>{
+    const data = await db.query.courses.findMany();
+
+    return data ;
+});
+
+export const getUserProgress = cache( async ()=> {
+    const { userId } = await auth();
+    
+    if(!userId){
+        return null ;
+    }
+    const data = await db.query.userProgress.findFirst({
+        where:eq(userProgress.userId , userId),
+        with :{
+            activeCourse:true,
+        }
+    })
+    return data;
+})
--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,6 @@
 
-import { serial, pgTable, text } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { serial, pgTable, text, integer } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses",{
     id: serial("id").primaryKey(),
@@ -7,6 +8,22 @@ export const courses = pgTable("courses",{
     imgSrc: text("image_src").notNull(),
 });
 
+export const coursesRelations = relations(courses,({many})=>({
+    userProgress : many(userProgress),
+}))
 
+export const userProgress = pgTable("user_progress",{
+    userId: text("user_id").primaryKey(),
+    userName : text("user_name").notNull().default("User"),
+    userImageSrc :text("user_image_src").notNull().default("/mascot.svg"),
+    activeCourseId : integer("active_course_id").references(()=>courses.id ,{onDelete:"cascade"}),
+    hearts :integer("hearts").notNull().default(5),
+    points : integer("points").notNull().default(0),
+})
 
-
+export const userProgressRelations = relations(userProgress,({one})=>({
+    activeCourse:one(courses ,{
+        fields :[userProgress.activeCourseId],
+        references:[courses.id],
+    })
+}))
